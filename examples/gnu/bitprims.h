95b226ec284ed5f02c3327c376a2106079c0c802
David Bremner <dbremner@gmail.com>
2015-10-10 17:22:37 -0700

    remove pre-ANSI debris

--- examples/gnu/bitprims.h
+++ examples/gnu/bitprims.h
@@ -63,20 +63,6 @@ typedef unsigned long _BS_word;
 #endif
 #endif
 
-#ifndef __P
-#ifdef __STDC__
-#define __P(protos) protos
-#else
-#define __P(protos) ()
-#endif
-#endif /*!__P*/
-#if !defined(__STDC__) && !defined(const)
-#define const
-#endif
-#if !defined(__STDC__) && !defined(void)
-#define void int
-#endif
-
 /* The 16 2-operand raster-ops:
    These match the correspodning GX codes in X11. */
 enum _BS_alu {
@@ -104,18 +90,18 @@ enum _BS_alu {
 extern "C" {
 #endif
 
-extern void _BS_and __P((_BS_word*,int, const _BS_word*, int, _BS_size_t));
-extern void _BS_blt __P((enum _BS_alu,
-			     _BS_word*,int, const _BS_word*,int, _BS_size_t));
-extern void _BS_copy __P((_BS_word*,int, const _BS_word*,int, _BS_size_t));
+extern void _BS_and (_BS_word*,int, const _BS_word*, int, _BS_size_t);
+extern void _BS_blt (enum _BS_alu,
+			     _BS_word*,int, const _BS_word*,int, _BS_size_t);
+extern void _BS_copy (_BS_word*,int, const _BS_word*,int, _BS_size_t);
 #define _BS_copy_0(DS, SS, LENGTH) _BS_copy(DS, 0, SS, 0, LENGTH)
-extern int _BS_count __P((const _BS_word*, int, _BS_size_t));
-extern int _BS_any __P((const _BS_word*, int, _BS_size_t));
-extern void _BS_clear __P((_BS_word*, int, _BS_size_t));
-extern void _BS_set __P((_BS_word*, int, _BS_size_t));
-extern void _BS_invert __P((_BS_word*, int, _BS_size_t));
-int _BS_lcompare_0 __P((_BS_word*, _BS_size_t, _BS_word*, _BS_size_t));
-extern void _BS_xor __P((_BS_word*,int, const _BS_word*,int, _BS_size_t));
+extern int _BS_count (const _BS_word*, int, _BS_size_t);
+extern int _BS_any (const _BS_word*, int, _BS_size_t);
+extern void _BS_clear (_BS_word*, int, _BS_size_t);
+extern void _BS_set (_BS_word*, int, _BS_size_t);
+extern void _BS_invert (_BS_word*, int, _BS_size_t);
+int _BS_lcompare_0 (_BS_word*, _BS_size_t, _BS_word*, _BS_size_t);
+extern void _BS_xor (_BS_word*,int, const _BS_word*,int, _BS_size_t);
 
 #ifdef __cplusplus
 }
